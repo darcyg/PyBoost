a32d2ad79ea10f99c2afb28c1732681994c54249
Sarat Tallamraju <sarattallamraju@gmail.com>
2013-05-13 18:03:17 -0700

    data object complete

--- projects/dataview/dataview.h
+++ projects/dataview/dataview.h
@@ -5,97 +5,82 @@
 #include <iostream>
 #include <string>
 #include <boost/format.hpp>
+#include <stdexcept>      // std::out_of_range
 
 using namespace std;
 
 namespace vfp {
 
-    class Data {
+    class Mat {
         public:
             uint8_t* data; 
-            int m_rows;
-            int m_cols;
+            int rows;
+            int cols;
 
-            Data() {}
+            Mat() {}
 
             // init data with stuff
-            Data(uint8_t* _data, int _rows, int _cols) {
+            Mat(uint8_t* _data, int _rows, int _cols) {
                 data = _data;
-                m_rows = _rows;
-                m_cols = _cols;
+                rows = _rows;
+                cols = _cols;
             }
 
             // init data and fill in each cell with its index
-            Data(int _rows, int _cols) {
-                m_rows = _rows;
-                m_cols = _cols;
+            Mat(int _rows, int _cols) {
+                rows = _rows;
+                cols = _cols;
                 data = new uint8_t[_rows*_cols]();
 
-                for (int i = 0; i < m_rows; i++) {
-                    for (int j = 0; j < m_cols; j++) {
-                        int index = i * m_cols + j;
+                for (int i = 0; i < rows; i++) {
+                    for (int j = 0; j < cols; j++) {
+                        int index = i * cols + j;
                         data[index] = (uint8_t)index;
                     }
                 } 
             };
 
+            // Mimic the cv::Mat way of accessing specific element
+            template<typename T> 
+            T& at(int y, int x) {
+                checkDimensions<T>(y,x);
+                T* d = view<T*>();
+                int index = x + y * (cols/sizeof(T));
+                return d[index];
+            }
+
+            // casts by the template arg
+            template<typename T>
+            T view() {
+                return reinterpret_cast<T>(data);
+            }
+
             // print the data
+            template<typename T>
             void print() {
+                T* m_data = view<T*>();
+                int m_rows = rows;
+                int m_cols = cols/sizeof(T);
+
                 cout << "\n[" << endl;
                 for (int i = 0; i < m_rows; i++) {
                     cout << "\t[ ";
                     for (int j = 0; j < m_cols; j++) {
                         int index = i * m_cols + j;
-                        cout << boost::format("%d,\t") % (int)data[index];
+                        cout << boost::format("%s,\t") % (int)m_data[index];
                     }
                     cout << "],\n" << endl;
                 }
                 cout << "]" << endl;
             }
-    };
+        protected:
+            template<typename T> void checkDimensions(int y, int x) {
+                // check if the row and column index is valid
+                int maxRowIndex = rows - 1;
+                int maxColIndex = cols/sizeof(T) - 1;
 
-    template <class T>
-    class DataView : public Data {
-        public:
-
-            int cols() { return m_cols/sizeof(T); }
-            int rows() { return m_rows; }
-            void rows(int y) { m_rows = y; }
-            void cols(int x) { m_cols = x; }
-
-            T* view() {
-                return reinterpret_cast<T*>(data);
-            }
-
-            int get(int y, int x) {
-                T* m_data = view();
-                int index = x + y * cols();
-                return m_data[index];
-            }
-            int get(int y) {
-                T* m_data = view();
-                int index = y * cols();
-                return m_data[index];
-            }
-            int set(int y, int x, T value) {
-                T* m_data = view();
-                int index = x + y * cols();
-                m_data[index] = value;
-            }
-
-            void print() {
-                T* m_data = view();
-
-                cout << "\n[" << endl;
-                for (int i = 0; i < rows(); i++) {
-                    cout << "\t[ ";
-                    for (int j = 0; j < cols(); j++) {
-                        int index = i * cols() + j;
-                        cout << boost::format("%d,\t") % m_data[index];
-                    }
-                    cout << "],\n" << endl;
-                }
-                cout << "]" << endl;
+                if (y > maxRowIndex) { throw std::out_of_range((boost::format("vfp::Mat row index out of bounds: %d > %d") % y % maxRowIndex).str()); }
+                if (x > maxColIndex) { throw std::out_of_range((boost::format("vfp::Mat col index out of bounds: %d > %d") % x % maxColIndex).str()); }
             }
     };
 
--- projects/dataview/main.cpp
+++ projects/dataview/main.cpp
@@ -11,46 +11,23 @@ using namespace vfp;
 int main() {
     cout << "Starting" << endl;
 
-    //Data d = Data(12,24);
-    //d.print();
-   
-    //DataView<uint32_t>* data = new DataView<uint32_t>(12, 6, 0);    
-    //data->print();
+    Mat* d = new Mat(8,16);
+    d->print<uint8_t>();
+    d->print<uint32_t>();
 
-    Data* d = new Data(8,16);
-    d->print();
+    cout << "\n-------------------------------" << endl;
+    cout << "Getting element 5,3 as uint32_t" << endl;
+    cout << d->at<uint32_t>(5,3) << endl;
 
-    DataView<uint32_t>* newData = (DataView<uint32_t>*)d;
-    cout << newData->m_rows << endl;
-    cout << newData->m_cols << endl;
+    cout << "\n-------------------------------" << endl;
+    cout << "Getting element 3,2 as uint32_t" << endl;
+    cout << d->at<uint32_t>(3,2) << endl;
 
-    cout << "-------------" << endl;
+    cout << "\n-------------------------------" << endl;
+    cout << "Setting element 3,2 as uint32_t" << endl;
+    d->at<uint32_t>(3,2) = 7;
 
-    cout << newData->rows() << endl;
-    cout << newData->cols() << endl;
-
-    cout << "-------------" << endl;
-    newData->print();
-
-    cout << "-------------" << endl;
-    cout << "Getting element 5, 3" << endl;
-    cout << newData->get(5,3) << endl;
-
-    cout << "-------------" << endl;
-    cout << "Setting element 5, 3 to 50462976" << endl;
-    newData->set(5,3, 50462976);
-
-    cout << "-------------" << endl;
-    cout << "Getting element 5, 3 again" << endl;
-    cout << newData->get(5,3) << endl;
-
-    cout << "-------------" << endl;
-    newData->print();
-
-    cout << "-------------" << endl;
-    cout << "The Data object should also change" << endl;
-    d->print();
-
+    d->print<uint32_t>();
 
     cout << "Done" << endl;
     return 0;
